391ec905cc65c4ae9bc85074aed471e1b8ae61bb
yamilemlf <yamilemlessa@gmail.com>
2023-02-06 20:13:12 -0300

    remove not used route GET /eligibilityTest

--- app.js
+++ app.js
@@ -6,11 +6,7 @@ var jsonParser = bodyParser.json()
 const port = 3000
 const { eligibilityController } = require('./controllers/eligibilityController.js')
 
-app.use("/eligibilityTest", router.post("/", jsonParser, eligibilityController.post ))
-
-app.get('/eligibilityTest', (req, res) => {
-  res.send('Teste de elegibilidade')
-})
+app.use("/eligibilityTest", router.post("/", jsonParser, eligibilityController.post))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
