ea897c397944a25036d1cbe87c5fcb94dd142d0c
yamilemlf <yamilemlessa@gmail.com>
2023-02-07 20:19:28 -0300

    lint fixes

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const router = express.Router()
 const bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
 const port = 3000
-const { eligibilityController } = require('./controllers/eligibilityController.js')
+const { eligibilityController } = require('./src/controllers/eligibilityController')
 
-app.use("/eligibilityTest", router.post("/", jsonParser, eligibilityController.post))
+app.use('/eligibilityTest', router.post('/', jsonParser, eligibilityController.post))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
